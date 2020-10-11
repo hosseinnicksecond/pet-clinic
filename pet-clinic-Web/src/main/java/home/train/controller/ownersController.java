45f0914010afcb8123943dd8a54a84e92303bf56
hossein nick <hossinnick62@gmail.com>
2020-10-11 23:35:16 +0330

    add table to list.html for owner and manage controller

--- pet-clinic-Web/src/main/java/home/train/controller/ownersController.java
+++ pet-clinic-Web/src/main/java/home/train/controller/ownersController.java
@@ -1,14 +1,24 @@
 package home.train.controller;
 
+import home.train.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class ownersController {
 
+    private final OwnerService service;
+
+    public ownersController(OwnerService service) {
+        this.service = service;
+    }
+
     @RequestMapping({"","/list"})
-    public String getOwnerList(){
+    public String getOwnerList(Model model){
+
+        model.addAttribute("owners",service.findAll());
         return "owners/list";
     }
 }
