ea017988cba592d42167e9a04276c7469ac4aebd
hossein nick <hossinnick62@gmail.com>
2020-10-11 23:43:29 +0330

    add table to list.html for Vet and manage controller

--- pet-clinic-Web/src/main/java/home/train/controller/vetsController.java
+++ pet-clinic-Web/src/main/java/home/train/controller/vetsController.java
@@ -1,14 +1,25 @@
 package home.train.controller;
 
+import home.train.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vets")
 @Controller
 public class vetsController {
 
+    private final VetService service;
+
+
+    public vetsController(VetService service) {
+        this.service = service;
+    }
+
     @RequestMapping({"","/list"})
-    public String vetList(){
+    public String vetList(Model model){
+
+        model.addAttribute("vets",service.findAll());
         return "vets/list";
     }
 }
