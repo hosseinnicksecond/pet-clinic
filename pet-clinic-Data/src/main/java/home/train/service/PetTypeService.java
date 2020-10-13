0424014bf39121f744d0c6f22730f7803197a8b5
hossein nick <hossinnick62@gmail.com>
2020-10-13 23:31:42 +0330

    create PetTypeMapService . Closes #17

---
+++ pet-clinic-Data/src/main/java/home/train/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package home.train.service;
+
+import home.train.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-Data/src/main/java/home/train/service/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package home.train.service.map;
+
+import home.train.model.PetType;
+import home.train.service.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService  {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType t) {
+        return super.save(t);
+    }
+
+    @Override
+    public void delete(PetType t) {
+      super.delete(t);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+       super.deleteById(id);
+    }
+}
