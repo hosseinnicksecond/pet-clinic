76d4e988d71e2527ce620c84912cfca971018342
hossein nick <hossinnick62@gmail.com>
2020-10-13 23:17:52 +0330

    create Speciality and vet Speciality set. Closes #16

---
+++ pet-clinic-Data/src/main/java/home/train/model/Speciality.java
@@ -0,0 +1,14 @@
+package home.train.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-Data/src/main/java/home/train/model/Vet.java
+++ pet-clinic-Data/src/main/java/home/train/model/Vet.java
@@ -1,4 +1,16 @@
 package home.train.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Speciality> specialitySet;
+
+    public Set<Speciality> getSpecialitySet() {
+        return specialitySet;
+    }
+
+    public void setSpecialitySet(Set<Speciality> specialitySet) {
+        this.specialitySet = specialitySet;
+    }
 }
